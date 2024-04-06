13f77c995fc8b2919c540b07fea6b11dbd7287d3
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2024-04-06 15:18:30 +0200

    Test + heatmap

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,19 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if titles and descriptions are fine', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.card-description h1').should('be.visible').and('contain', 'Rome');
+    cy.get('.card-description p').should('be.visible').and('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.card-description h1').should('be.visible').and('contain', 'London');
+    cy.get('.card-description p').should('be.visible').and('contain', 'United Kingdom');
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click();
+    cy.get('.card-description h1').should('be.visible').and('contain', 'Paris');
+    cy.get('.card-description p').should('be.visible').and('contain', 'France');
+  });
+});
